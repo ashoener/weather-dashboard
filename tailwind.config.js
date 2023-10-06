58f052218e63c69e692bb0bdb9aba660392dd6c1
Andrew Shoener <a.b.shoener@gmail.com>
2023-10-06 17:43:11 -0500

    Add fonts Start work on html

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      header: ["Roboto", "sans-serif"],
+      subheader: ["Hind Vadodara", "sans-serif"],
+      content: ["Mako", "sans-serif"],
+    },
   },
   plugins: [],
 };
