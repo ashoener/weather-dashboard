32c0be6c7fcc3b40daec891ba086eda6649668cb
Andrew Shoener <a.b.shoener@gmail.com>
2023-10-06 20:06:44 -0500

    Added lat/long caching

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,16 +2,39 @@
 var Alpine = window.Alpine;
 
 const apiKey = "e229231d710a9cebff255e2b499ccac6";
+let coordinateCache = JSON.parse(localStorage.getItem("coordinateCache")) || {};
 
 document.addEventListener("alpine:init", () => {
   let recentCities = JSON.parse(localStorage.getItem("recentCities")) || [];
   Alpine.store("data", { recentCities, currentCity: "" });
 });
 
-function search() {
+async function getLatLong(location) {
+  if (location in coordinateCache) return coordinateCache[location];
+  const coords = await fetch(
+    `https://api.openweathermap.org/geo/1.0/direct?q=${encodeURIComponent(
+      location
+    )}&limit=1&appid=${apiKey}`
+  )
+    .then((res) => res.json())
+    .then((data) =>
+      data.length ? { lat: data[0].lat, lon: data[0].lon } : false
+    );
+  if (coords) {
+    coordinateCache[location] = coords;
+    localStorage.setItem("coordinateCache", JSON.stringify(coordinateCache));
+  }
+  return coords;
+}
+
+async function search(location = "") {
   const data = Alpine.store("data");
-  if (!data.currentCity.length) return;
-  data.recentCities.push(data.currentCity);
+  if (!location.length) location = data.currentCity;
+  if (!location) return;
+  const loc = await getLatLong(location);
+  if (!data.recentCities.includes(location)) {
+    data.recentCities.push(location);
+    localStorage.setItem("recentCities", JSON.stringify(data.recentCities));
+  }
   data.currentCity = "";
-  localStorage.setItem("recentCities", JSON.stringify(data.recentCities));
 }
